48a6d3959f0a6c83e90391f3d767c74a328e0052
aperezm <aperezm@e968f4be-1634-4921-b96b-e3dd0a78059a>
2011-05-04 14:31:00 +0000

    Refactoring modules to 1.3

--- lib/IWmain/Installer.php
+++ lib/IWmain/Installer.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * PostNuke Application Framework
- *
- * @copyright (c) 2002, PostNuke Development Team
- * @link http://www.postnuke.com
- * @version $Id: pninit.php 22139 2007-06-01 10:57:16Z markwest $
- * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
- * @package PostNuke_Value_Addons
- * @subpackage Webbox
- */
 class IWmain_Installer extends Zikula_AbstractInstaller
 {
     /**
